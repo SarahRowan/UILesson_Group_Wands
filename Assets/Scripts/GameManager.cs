5cb7adf6a29520ada386f17832ad3d631b19082e
Sarah Rowan <ninjasarahb@gmail.com>
2019-08-11 22:04:09 -0400

    Updated comments and debug logs to be more helpful

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; // Make sure to add this Using statement in any script accessing the UI!
 
+// This class handles running the actual game. 
 public class GameManager : MonoBehaviour
 {
+	// the node the player is currently on
 	private Node currentNode;
+
+	// a list of nodes in the game
 	private Node node1;
 	private Node node2;
 
+	// references to the main and button text gameobjects, to update their texts 
 	public GameObject mainText;
 	public GameObject leftButtonText;
 
 	// Start is called before the first frame update
     void Start()
     {
+		// initialize all your nodes here, with their proper texts and references. 
+		// initialize them in REVERSE order! last nodes first. this prevents the nodes from being null
+		// when referenced inside other nodes.
 		node2 = new Node("Oh no, a witch!", "Fight her!", "Give her your hair", null, null);
 		node1 = new Node("You're walking through the woods", "LOOK FOR DANGER", "Chill out", node2, node2);
 
+		// set the currentNode to the beginning and run the first Update to make sure everything is set up
 		currentNode = node1;
 		UpdateText(currentNode);
     }
@@ -28,18 +37,23 @@ public class GameManager : MonoBehaviour
         
     }
 
-	public void UpdateText(Node newNode) {
+	// This method moves the currentNode the node passed in through newNode
+	// then updates all text with the text in newNode
+	private void UpdateText(Node newNode) {
 		currentNode = newNode;
 		mainText.GetComponent<Text>().text = currentNode.mainText;
 		leftButtonText.GetComponent<Text>().text = currentNode.leftOption;
 	}
 
+	// This method is called when the left button is clicked. 
+	// It is hooked up in the editor. 
+	// If there is a valid left node, UpdateText is called. Otherwise, it only prints to the console.
 	public void ClickLeft() {
-		Debug.Log("Got it!");
+		Debug.Log("Left Node Clicked!");
 		if(currentNode.leftNode != null) {
 			UpdateText(currentNode.leftNode);
 			Debug.Log("updating...");
 		}
-		else{ Debug.Log("ummmmmm" + currentNode); }
+		else{ Debug.Log("no node available"); }
 	}
 }
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿// This class is NOT a monobehavior! That's because we aren't using any unity-specific systems.
+// This node just holds data, so the game knows what to load when the player clicks. 
+// It's part of a structure called Linked Lists, which just means a class that takes an object of its own type as a parameter. 
+// We use these to make paths in our data... like we need to write narrative games!
 public class Node
 {
+	// this syntax, with the comma-separated names, lets us initialize lots of variables of the same type at once.
+	// mainText, leftOption, and rightOption are all public strings.
 	public string mainText, leftOption, rightOption;
+
+	//leftNode and rightNode are both Nodes.
 	public Node leftNode, rightNode;
 
+	// This is a constructor! It's just like we used in Processing. This constructor allows our Node class
+	// to take in parameters. 
+	// Using "p_" in front of a variable is fairly common best practice. It lets us name our parameters the same
+	// as the values they're going to fill, without the risk of confusing them. 
 	public Node(string p_mainText, string p_leftOption, string p_rightOption, Node p_leftNode, Node p_rightNode) {
 		mainText = p_mainText;
 		leftOption = p_leftOption;
