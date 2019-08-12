bd07302bedf98ca2fa824054f7f1b5ee227a05cd
Sarah Rowan <ninjasarahb@gmail.com>
2019-08-11 21:20:36 -0400

    Initial Commit. Make a branch to get started

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	private Node currentNode;
+	private Node node1;
+	private Node node2;
+
+	public GameObject mainText;
+	public GameObject leftButtonText;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+		node2 = new Node("Oh no, a witch!", "Fight her!", "Give her your hair", null, null);
+		node1 = new Node("You're walking through the woods", "LOOK FOR DANGER", "Chill out", node2, node2);
+
+		currentNode = node1;
+		UpdateText(currentNode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void UpdateText(Node newNode) {
+		currentNode = newNode;
+		mainText.GetComponent<Text>().text = currentNode.mainText;
+		leftButtonText.GetComponent<Text>().text = currentNode.leftOption;
+	}
+
+	public void ClickLeft() {
+		Debug.Log("Got it!");
+		if(currentNode.leftNode != null) {
+			UpdateText(currentNode.leftNode);
+			Debug.Log("updating...");
+		}
+		else{ Debug.Log("ummmmmm" + currentNode); }
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public string mainText, leftOption, rightOption;
+	public Node leftNode, rightNode;
+
+	public Node(string p_mainText, string p_leftOption, string p_rightOption, Node p_leftNode, Node p_rightNode) {
+		mainText = p_mainText;
+		leftOption = p_leftOption;
+		rightOption = p_rightOption;
+		leftNode = p_leftNode;
+		rightNode = p_rightNode;
+	}
+}
