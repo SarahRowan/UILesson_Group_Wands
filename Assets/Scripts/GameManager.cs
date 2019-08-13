25ee3939dddc0bbafc5bba8e90f59225908cf837
soozical <susie.odneal@gmail.com>
2019-08-13 14:45:42 -0500

    updated nodes 6, 7, 8's text.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
 	// a list of nodes in the game
 	private Node node1;
 	private Node node2;
+	private Node node6;
+	private Node node7;
+	private Node node8;
 
 	// references to the main and button text gameobjects, to update their texts 
 	public GameObject mainText;
@@ -23,6 +26,9 @@ public class GameManager : MonoBehaviour
 		// initialize all your nodes here, with their proper texts and references. 
 		// initialize them in REVERSE order! last nodes first. this prevents the nodes from being null
 		// when referenced inside other nodes.
+		node8 = new Node ("In your very rude hurry, you slip and fall into a deep hole, filled with nameless beasts who harvest souls.", "Fight them!", "Climb back out!", node4, node4);
+		node7 = new Node("Oops. You fall in and drown.", "Try to swim?", node4, node4);
+		node6 = new Node ("Sploosh, sploosh, sploosh. Everything is wet and terrible. But it could have been worse. You could have died or something.", "Do it again", "Maybe tomorrow it won't rain...", node1, node1);
 		node2 = new Node("Oh no, a witch!", "Fight her!", "Give her your hair", null, null);
 		node1 = new Node("You're walking through the woods", "LOOK FOR DANGER", "Chill out", node2, node2);
 
