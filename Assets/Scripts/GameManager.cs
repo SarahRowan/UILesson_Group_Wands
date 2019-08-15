62a123c1cfeee9fdc193d9cda42b2179ba19d7f7
[aecroft] <amycroft@AmyCrofComputer.lan>
2019-08-15 19:33:14 +0100

    first commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,18 +12,31 @@ public class GameManager : MonoBehaviour
 	// a list of nodes in the game
 	private Node node1;
 	private Node node2;
+    private Node node3;
+    private Node node4;
+    private Node node5;
+    private Node node6;
+    private Node node7;
+    private Node node8;
 
-	// references to the main and button text gameobjects, to update their texts 
-	public GameObject mainText;
+    // references to the main and button text gameobjects, to update their texts 
+    public GameObject mainText;
 	public GameObject leftButtonText;
 
 	// Start is called before the first frame update
     void Start()
     {
-		// initialize all your nodes here, with their proper texts and references. 
-		// initialize them in REVERSE order! last nodes first. this prevents the nodes from being null
-		// when referenced inside other nodes.
-		node2 = new Node("Oh no, a witch!", "Fight her!", "Give her your hair", null, null);
+        // initialize all your nodes here, with their proper texts and references. 
+        // initialize them in REVERSE order! last nodes first. this prevents the nodes from being null
+        // when referenced inside other nodes.
+
+        node8 = new Node("In your very rude hurry, you slip and fall into a deep hole, filled with nameless beasts who harvest souls.", "Fight them!", "Climb back out!", node4, node4);
+        node7 = new Node("Oops. You fall in and drown.", "Ugh.", "Try to swim?", node4, node4);
+        node6 = new Node("Sploosh, sploosh, sploosh. Everything is wet and terrible. But it could have been worse. You could have died or something.", "Do it again", "Maybe tomorrow it won't rain...", node1, node1);
+        node5 = new Node("She accepts your gift and decides not to curse you. ", "Hurry away quickly", "Continue trudging in the rain", node8, node1);
+        node4 = new Node("Great, you died. Now your day is even worse", "Start over", "Try again", node1, node1);
+        node3 = new Node("You keep your head down, trying to avoid getting rain in your eyes. There's a huge puddle with a log-bridge in your way.", "Cross the bridge", "Go around", node7, node6); 
+        node2 = new Node("Oh no, a witch!", "Fight her!", "Give her your hair", node4, node5);
 		node1 = new Node("You're walking through the woods", "LOOK FOR DANGER", "Chill out", node2, node2);
 
 		// set the currentNode to the beginning and run the first Update to make sure everything is set up
